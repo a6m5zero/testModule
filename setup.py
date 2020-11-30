3bf843866ef6e5555aff7abbc91e61e2ffcacfe7
Dim <Dim@mail.ru>
2020-11-30 22:19:43 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "mycalc",
+    version = "0.0.1",
+    description = "Example module with arithpmetic operations",
+    packages = find_packages()
+    include_package_data = True, 
+    author = 'Dumbass'
+    author_email = 'Dumbass@mail.com'
+    url = 'https://github.com/a6m5zero/testModule',
+)
+
